190dc5905ba3e039fba80adbcfe05f89913b1666
AJAY KUMAR <114848454+Aj3322@users.noreply.github.com>
2024-04-09 22:29:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
-// Require your database connection file
 require("./db/conn");
-
-// Import your routes
+const app = express();
 const routes = require("./router/router");
-
-// Use middleware to parse JSON bodies
-app.use(express.json());
+const path = require("path");
 app.use(express.json());
 app.use(routes);
+const PORT = process.env.PORT || 3000;
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Handle 404 errors
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
